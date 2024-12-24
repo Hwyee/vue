ededdbb8b06080a0c52da9a584ab494f8b5b0fe7
hwyee <hwyee@foxmail.com>
2024-12-25 00:30:33 +0800

    xgplayer better.

--- hellovue/src/router/index.js
+++ hellovue/src/router/index.js
@@ -17,6 +17,14 @@ const router = createRouter({
             // which is lazy-loaded when the route is visited.
             component: () => import('../components/optionaltest.vue')
         },
+        {
+            path: '/obs',
+            name: 'obs-streaming',
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import('../components/player/Myxgplayer.vue')
+        },
         // {
         //     path: '/user/:name',
         //     name: 'User',
